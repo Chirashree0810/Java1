b92ca55b0c397e6b9984bc16d96f17cc5a48bc6e
Chirashree0810 <chirashree.23cs031@sode-edu.in>
2024-10-15 15:25:30 +0530

    USN IA1

--- C20.java
+++ C20.java
@@ -1,21 +1,11 @@
-import java.util.Scanner;
-
 public class C20 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter USN (without hyphens): ");
-        String usn = scanner.next();
+        String prefix = "4MW23CS";
+        int start = 1;
+        int end = 65;
 
-        if (usn.length() == 10) {
-            System.out.println("USN with hyphens: ");
-            for (int i = 0; i < usn.length(); i++) {
-                System.out.print(usn.charAt(i));
-                if (i == 2 || i == 5) {
-                    System.out.print("-");
-                }
-            }
-        } else {
-            System.out.println("Invalid USN length. Please enter 10 characters.");
+        for (int i = start; i <= end; i++) {
+            System.out.println(prefix + String.format("%03d", i));
         }
     }
 }
