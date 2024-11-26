f6da05470a110b8e83d916e821e24dc437b49975
Chirashree0810 <chirashree.23cs031@sode-edu.in>
2024-11-26 15:18:16 +0530

    factorial of a number

---
+++ C44.java
@@ -0,0 +1,19 @@
+public class C44 {
+    // Recursive method to calculate factorial
+    public static int factorial(int n) {
+        // Base case: factorial of 0 or 1 is 1
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        // Recursive call: n! = n * (n-1)!
+        else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is " + result);
+    }
+}
